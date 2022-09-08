c119f3fecfe2339495d5c9dd1aaf74496dffa431
Kevin Willian <kevinwillian084@gmail.com>
2022-09-08 17:58:54 -0300

    feat: add req 05

---
+++ challenges/desafio05.js
@@ -0,0 +1,10 @@
+db.produtos
+  .find(
+    {
+      $or: [
+        { curtidas: 36 },
+        { vendidos: 85 },
+      ],
+    },
+    { _id: false, nome: true, curtidas: true, vendidos: true },
+  );
